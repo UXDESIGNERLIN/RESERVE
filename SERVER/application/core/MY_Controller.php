abb97a59c585a97df2df5013f56c55d3523bfe5d
Ignasi <ignasimg@gmail.com>
2019-03-08 12:00:03 +0100

    SERVER :: Setup composer & library for phone numbers

--- SERVER/application/core/MY_Controller.php
+++ SERVER/application/core/MY_Controller.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-//require APPPATH.'third_party/autoload.php';
+require APPPATH.'third_party/autoload.php';
 //require 'v0/Traits.php';
 require 'v0/MY_Controller.php';
