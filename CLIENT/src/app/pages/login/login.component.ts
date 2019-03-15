f566b74bc8d723ccb09451572c9d4aa7e0ef47bf
Chialingho <hochialing91@gmail.com>
2019-03-15 18:32:35 +0100

    CLIENT: AFTER LOGIN DIRECT TO MAIN

--- CLIENT/src/app/pages/login/login.component.ts
+++ CLIENT/src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Company } from 'src/app/interfaces/company';
+import { Session } from 'src/app/interfaces/session';
+import { SessionService } from 'src/app/services/session.service';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +9,29 @@ import { Company } from 'src/app/interfaces/company';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private sessionService: SessionService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
   }
+
+  login(email:string, password: string): void {
+    console.log("x");
+    this.sessionService.login(email, password).subscribe(
+      x => {
+        if (x.loggedIn) {
+          this.router.navigateByUrl("/main");//if the user is logged in, direct user to "./main" 
+        }
+        
+      }
+    )
+  }
+  
+  /*
+  login(email:string, password: string):Observable<Session> {
+    return this.apiservice.post(sessionurl, {password, email});
+  }
+  */
 
 }
