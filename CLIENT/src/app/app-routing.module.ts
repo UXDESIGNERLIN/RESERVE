ba361030bed0c52c288e235a6bf24731d3f89595
Chialingho <hochialing91@gmail.com>
2019-03-16 20:27:28 +0100

    CLIENT:  CREATE COURSELIST

--- CLIENT/src/app/app-routing.module.ts
+++ CLIENT/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { SignupComponent } from 'src/app/pages/signup/signup.component';
 import { LogoutComponent } from 'src/app/pages/logout/logout.component';
 import { LoginComponent } from 'src/app/pages/login/login.component';
 import { MainComponent } from 'src/app/pages/main/main.component';
+import { CourseslistComponent } from './pages/courseslist/courseslist/courseslist.component';
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full'},
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'logout', component: LogoutComponent },
-  { path: 'main', component: MainComponent }
+  { path: 'main', component: MainComponent },
+  { path: 'courseslist', component: CourseslistComponent }
 ];
 
 @NgModule({
