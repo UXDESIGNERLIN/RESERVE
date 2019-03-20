612baed6add20285d50992a8432661d9793c8c99
Ignasi <ignasi@ausva04.com>
2019-03-20 21:48:26 +0100

    MAIN :: Added sections and general content

--- MAIN/src/assets/css/custom.js
+++ MAIN/src/assets/css/custom.js
@@ -1,2 +1,2 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './custom.css';
+import './custom.css'; 
--- MAIN/src/assets/js/index.js
+++ MAIN/src/assets/js/index.js
@@ -1,3 +1,25 @@
 //require('bootstrap');
+let panes = Array.from(document.getElementsByClassName('tab-pane')).reduce((p, c) => ({...p, [c.id]: c}), {});
 
-console.log('Hello!')
+console.log(panes);
+
+
+function hideAll() {
+    Object.keys(panes).forEach(id => {
+        panes[id].classList.remove('show');
+        panes[id].classList.remove('active');
+    })
+}
+
+function show(id) {
+    panes[id].classList.add('show');
+    panes[id].classList.add('active');
+}
+
+window.onhashchange = (e) => {
+    let url = e.newURL;
+    let hash = url.slice(url.lastIndexOf('#')+1);
+
+    hideAll();
+    show(hash);
+}
