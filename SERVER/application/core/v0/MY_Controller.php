UXDESIGNERLIN/RESERVE

commit c1eba267dfeea1eedfe6fef87ece884a1662223c

SERVER : CORS OPTIONS now replies whatever host did the request

SERVER/application/core/v0/MY_Controller.php
@@ -12,9 +12,12 @@ class MY_Controller extends CI_Controller {
     parent::__construct();
 
     date_default_timezone_set('Europe/Madrid');
+    
+    //var_dump($_SERVER);
 
     if (ENVIRONMENT !== 'production') {
-      header("Access-Control-Allow-Origin: http://localhost:4200");
+      header("Access-Control-Allow-Origin: ".$_SERVER['HTTP_ORIGIN']);
+      //header("Access-Control-Allow-Origin: http://localhost:4200");
       //header("Access-Control-Allow-Origin: *");
       header("Access-Control-Allow-Credentials: true");
       header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");

commit 8a53d74cb1e8ece0b716009af4f816c2f40e6b67

SERVER : Fix bug introduced by c1eba26

SERVER/application/core/v0/MY_Controller.php
@@ -16,7 +16,8 @@ class MY_Controller extends CI_Controller {
     //var_dump($_SERVER);
 
     if (ENVIRONMENT !== 'production') {
-      header("Access-Control-Allow-Origin: ".$_SERVER['HTTP_ORIGIN']);
+      if (isset($_SERVER['HTTP_ORIGIN']))
+        header("Access-Control-Allow-Origin: ".$_SERVER['HTTP_ORIGIN']);
       //header("Access-Control-Allow-Origin: http://localhost:4200");
       //header("Access-Control-Allow-Origin: *");
       header("Access-Control-Allow-Credentials: true");