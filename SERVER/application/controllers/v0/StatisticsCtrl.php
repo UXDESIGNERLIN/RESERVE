9da3301462df812d34d0abdebb4982bc12c196fc
Ignasi <ignasi@ausva04.com>
2019-03-22 17:06:10 +0100

    SERVER : StatisticsCtrl

---
+++ SERVER/application/controllers/v0/StatisticsCtrl.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class StatisticsCtrl extends MY_Controller {
+  public function __construct () {
+    parent::__construct();
+
+    $this->API = [
+      'generic' => [
+        'GET' => [
+          'fn' => 'GETALL', 
+        ],
+      ],
+    ];
+  }
+
+  protected function GETALL () {
+    $this->load->model('v0/CompaniesMdl');
+    $this->load->model('v0/ReservesMdl');
+    $companies = $this->CompaniesMdl->count([], false);
+    $reserves = $this->ReservesMdl->count([], false);
+    $this->_success(['companies' => $companies, 'reserves' => $reserves]);
+  }
+}
