9b63c535df60b97e9cd0cef222cdcbcb2e6ff2d3
Chialingho <hochialing91@gmail.com>
2019-03-26 20:55:48 +0100

    CLIENT: SELECTOR CLASS (CHILD) OUTPUT AND EVENTRMITTER

--- CLIENT/src/app/components/selectcourse/selectcourse.component.ts
+++ CLIENT/src/app/components/selectcourse/selectcourse.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Course } from 'src/app/interfaces/course';
+import { CourseService } from 'src/app/services/course.service';
+import { SessionService } from 'src/app/services/session.service';
+import { NgModel } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-selectcourse',
@@ -6,11 +12,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./selectcourse.component.css']
 })
 export class SelectcourseComponent implements OnInit {
+  
+  selectedCourse: number = null;
+  @Output() selectEvent = new EventEmitter<number>();
 
-  constructor() { }
+  courses: Course[];
+  constructor(private courseService: CourseService,
+              private sessionService: SessionService) { }
 
   ngOnInit() {
+    this.getAll()
   }
+  getAll() {
+    this.sessionService.getSession().subscribe(
+      x => {
+        this.courseService.getFromCompany(x.companyId).subscribe(
+          x => this.courses = x
+        )
+      }
+    )
+  }
+
+  sendSelected() {
+    this.selectEvent.emit(this.selectedCourse);
+    console.log("kid",this.selectedCourse);
+  }
+
 /*
   getFromCourse(id:number): Observable<Class[]> {
     return this.apiservice.get(`${courseurl}/${id}/classes`);
--- CLIENT/src/app/pages/createclass/createclass.component.ts
+++ CLIENT/src/app/pages/createclass/createclass.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ClassesService } from 'src/app/services/classes.service';
 import { Class } from 'src/app/interfaces/class';
+import { Course } from 'src/app/interfaces/course';
+
 
 @Component({
   selector: 'app-createclass',
@@ -8,10 +10,31 @@ import { Class } from 'src/app/interfaces/class';
   styleUrls: ['./createclass.component.css']
 })
 export class CreateclassComponent implements OnInit {
+  courseId: number;
+
+  newClass: Class = {
+    id: null,
+    idCourse: this.courseId,
+    tsIni: (+(new Date())) + 3600, 
+    len: null,
+    spots: null
+  }
+  
   
   constructor(private classesService: ClassesService) { }
 
   ngOnInit() {
+    
+  }
+  receiveCourseId(Eventarg) {
+    this.courseId = Eventarg;
+    
+  }
+  updateOrCreate() {
+    this.classesService.createToCourse(this.courseId, this.newClass).subscribe(
+      x => console.log(x)
+    )
+    console.log(this.newClass);
   }
 
 /*
