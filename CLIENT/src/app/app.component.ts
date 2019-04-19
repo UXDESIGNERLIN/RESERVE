54802bcc1b12b358f0b56054bade898668bacea5
Chialingho <hochialing91@gmail.com>
2019-04-19 09:42:03 +0200

    CLIENT: COMPANY PAGE

--- CLIENT/src/app/app.component.ts
+++ CLIENT/src/app/app.component.ts
@@ -5,6 +5,20 @@ import { CourseService } from './services/course.service';
 import { Company } from './interfaces/company';
 import { Session } from './interfaces/session';
 import { APIService } from './services/API.service';
+import { AlertService } from './services/alert.service';
+//import * as kkk from 'datatables.net-bs4';
+import * as tinymce from 'tinymce';
+import 'tinymce/themes/silver/theme.js';
+
+//import 'tinymce/skins/content/default/content.css';
+//import 'tinymce/skins/ui/oxide/content.min.css';
+//import 'tinymce/skins/ui/oxide/skin.min.css';
+
+
+//import 'datatables.net-bs4';
+
+//declare var $: any;
+//var dt = kkk( window, $ );
 
 const fakecompany: Company = {
   id: 5,
@@ -22,10 +36,19 @@ export class AppComponent implements OnInit {
   constructor(
     private companyService: CompanyService,
     private sessionService: SessionService,
-    private courseService: CourseService
+    private courseService: CourseService,
+    //private any: AlertService
   ) {}
 
   ngOnInit(){
+    tinymce.init({
+      selector: 'textarea',  // change this value according to your HTML
+      plugin: 'a_tinymce_plugin',
+      a_plugin_option: true,
+      a_configuration_option: 400
+    });
+    //this.any.test();
+    console.log(tinymce);
     /*
     this.sessionService.login("ling@gmail.com", "lalala").subscribe(
       (r)=>{
--- CLIENT/src/app/pages/company/company.component.ts
+++ CLIENT/src/app/pages/company/company.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CompanyService } from 'src/app/services/company.service';
+import { SessionService } from 'src/app/services/session.service';
+import { Company } from 'src/app/interfaces/company';
 
 @Component({
   selector: 'app-company',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./company.component.css']
 })
 export class CompanyComponent implements OnInit {
+  company_info: Company = {
+    id: null,
+    email: "",
+    password:"",
+    name: ""
+  };
 
-  constructor() { }
+  constructor(private companyService: CompanyService,
+              private sessionService: SessionService) { }
 
   ngOnInit() {
+    this.showDetail();
   }
 
+  showDetail() {
+    return this.sessionService.getSession().subscribe( (session) => {
+      console.log("test the session: ",session);
+      return this.companyService.getSelf(session.companyId).subscribe( (company:Company) => {
+        this.company_info = company;
+        console.log("COMPANY INFO: ",company);
+      })
+    })
+  
+  }
+
+
+/*
+  showDetail(id:number) {
+    return this.companyService.getSelf(id).subscribe( (company) => {
+      console.log("Company Info: ",company);
+    })
+  }
+*/
 }
