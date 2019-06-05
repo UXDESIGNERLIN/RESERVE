0415785d4169a9e9c6aad56b44ac169430da8f09
Ignasi <ignasi@ausva04.com>
2019-06-05 14:58:34 +0200

    RESERVATION: Action feedback and error handling.

--- RESERVE/src/assets/scripts/utils.ts
+++ RESERVE/src/assets/scripts/utils.ts
@@ -66,11 +66,17 @@ export function getClass(classId: number | string): Promise<Class> {
 }
 
 export function register(classId:string | number,term:any) {
-  
-  let xhttp = new XMLHttpRequest();
-  xhttp.open("POST",`${base_api}/class/${classId}/reserves`);
-  xhttp.setRequestHeader('Content-type', 'application/json');
-  xhttp.send(JSON.stringify(term));
+  return APIRequest('POST', `${base_api}/class/${classId}/reserves`, term);
+
+  //return new Promise((resolve, reject) => {
+  //  let xhttp = new XMLHttpRequest();
+  //  xhttp.open("POST",`${base_api}/class/${classId}/reserves`);
+  //  xhttp.setRequestHeader('Content-type', 'application/json');
+  //  xhttp.send(JSON.stringify(term));
+  //  xhttp.onreadystatechange = function () {
+  //    
+  //  }
+  //});
 }
 
 function pad (v: number) {
@@ -86,3 +92,74 @@ export function hhmm(date: number) {
   let d = new Date(date*1000);
   return `${pad(d.getHours())}:${pad(d.getMinutes())}`;
 }
+
+function makeRequest (method: 'GET' | 'POST' | 'PUT', url: string, body: any) {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    if (body != null && typeof(body) == 'string') {
+      xhr.setRequestHeader('Content-type', 'text/plain'); //  application/x-www-form-urlencoded      multipart/form-data
+      xhr.send(body);
+    }
+    else if (body != null) {
+      xhr.setRequestHeader('Content-type', 'application/json');
+      xhr.send(JSON.stringify(body));
+    }
+    else {
+      xhr.send();
+    }
+    xhr.onload = function () {
+      if (this.status >= 200 && this.status < 300) {
+        resolve(xhr.responseText);
+      } else {
+        reject(xhr.responseText);
+        //reject({
+        //  status: this.status,
+        //  statusText: xhr.statusText,
+        //  response: xhr.responseText
+        //});
+      }
+    };
+    xhr.onerror = function () {
+      reject(`{success: false, code: "UNHANDLED_ERROR", data:""}`);
+      //reject({
+      //  status: this.status,
+      //  statusText: xhr.statusText,
+      //  response: xhr.responseText
+      //});
+    };
+  });
+}
+
+interface APIResponse {
+  success: boolean,
+  code: string,
+  data: any,
+}
+
+function APIRequest (method: 'GET' | 'POST' | 'PUT', url: string, body: any) {
+  
+  let parseJsonOrUnhandled = (r:string) => {
+    try {
+      return JSON.parse(r) as APIResponse;
+    }
+    catch (e) {
+      return {success: false, code: "UNHANDLED_ERROR", data:""} as APIResponse;
+    }
+  }
+
+  return makeRequest(method, url, body)
+    .then(parseJsonOrUnhandled)
+    .catch(parseJsonOrUnhandled)
+    .then((apiRes) => {
+      if (apiRes.success) return Promise.resolve(apiRes);
+      
+      alert(apiRes.code);
+
+      return Promise.reject(apiRes);
+    });
+}
+
+
+
+export const NOP = () => {};
