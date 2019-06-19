6fec17bef777758fbdd246b9a6d0d78b6fa0e0e2
Ignasi <ignasi@ausva04.com>
2019-06-19 11:35:35 +0200

    CLIENT: review code contact users  page

--- CLIENT/src/app/pages/contact-users/contact-users.component.ts
+++ CLIENT/src/app/pages/contact-users/contact-users.component.ts
@@ -4,8 +4,10 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { CourseService } from 'src/app/services/course.service';
 import { CompanyService } from 'src/app/services/company.service';
 import { AlertService } from 'src/app/services/alert.service';
-import { load } from '@angular/core/src/render3';
 import { ReservationService } from 'src/app/services/reservation.service';
+import { Observable } from 'rxjs';
+import { Course } from 'src/app/interfaces/course';
+import { Class } from 'src/app/interfaces/class';
 
 @Component({
   selector: 'app-contact-users',
@@ -14,113 +16,61 @@ import { ReservationService } from 'src/app/services/reservation.service';
 })
 export class ContactUsersComponent implements OnInit {
   id: string = this.activatedRoute.snapshot.paramMap.get("id");
+
   by: string = this.activatedRoute.snapshot.paramMap.get("by");
 
   member = {
     course_name: "",
     course_description: "",
-    class_time: null
-  }
-
-  member_show = {
-    course: false,
-    class: false
+    class_time: null,
+    show: false, 
+    label: ""
   }
 
   futureEngagement: boolean = false;
-  futureEngage: boolean = false;
-
-  all_members_class: boolean;
+  futureEngagementAvailable: boolean = false;
 
   subject: string;
   msgBody: string;
-  classId: number = 8;
-  constructor(
+  
+  constructor (
     private companyService: CompanyService,
     private classesService: ClassesService,
     private courseService: CourseService,
     private activatedRoute: ActivatedRoute,
-    private alertService: AlertService,
-    private reservationService: ReservationService,
-    private router: Router) { }
+    private alertService: AlertService,) { }
 
-  ngOnInit() {
+  ngOnInit () {
     this.load();
   }
 
-  load() {
-    this.member_show[this.by] = true;
-    console.log("futureEngage", this.futureEngage, this.by);
-    console.log("show", this.member_show);
-    if (this.by == "course") {
-      this.courseService.getById(this.id).subscribe(
-        (course) => {
-
-          this.member.course_name = course.name;
-          this.member.course_description = course.description;
-        }
-      );
+  load () {
+    if (this.by == "course" || this.by == "class") {
+      this.member.show = true;
+      this.member.label = this.by == "course" ? "Event" : "Schedule";
+      let service = this.by == "course" ? this.courseService : this.classesService;
+      (service.getById(this.id) as Observable<Course | Class>).subscribe( (classOrCourse: any) => {
+        this.member.course_name = classOrCourse.name;
+        this.member.course_description = this.by == "course" ? classOrCourse.description : null;
+        this.member.class_time = classOrCourse.tsIni;
+        this.futureEngagementAvailable = (classOrCourse.tsIni != null) && (classOrCourse.tsIni > ((+Date.now()/1000) | 0));
+      });
     }
-    else if (this.by == "class") {
-      this.futureEngage = true;
-      //FutureEngage will become false when 
-      this.classesService.getById(this.id).subscribe(
-        (classes) => {
-          //console.log("Date", Date.now());
-          this.member.class_time = classes.tsIni;
-          if(classes.tsIni <= ((+Date.now()/1000) | 0)) {
-            this.futureEngage = false;
-          }
-        }
-      );
-    }
-
-
   }
 
-
-
-  send() {
+  send () {
     if (this.by == 'company') {
-      this.companyService.engage(this.subject, this.msgBody).subscribe(
-        (x) => {
-          console.log("send to company all", x);
-        }
-      )
+      this.companyService.engage(this.subject, this.msgBody).subscribe()
     }
     else if (this.by == 'class') {
-
-      this.classesService.engage(this.id, this.subject, this.msgBody, this.futureEngagement).subscribe(
-        (x) => {
-          console.log("send to classes", x);
-        }
-      );
+      this.classesService.engage(this.id, this.subject, this.msgBody, this.futureEngagement).subscribe();
     }
     else if (this.by == 'course') {
       this.courseService.engage(this.id, this.subject, this.msgBody).subscribe();
     }
-
     else {
       this.alertService.error('Unknown');
     }
-
   }
-
-  tick(event) {
-    if (event) {
-      this.futureEngagement = true;
-    }
-    console.log(event);
-
-
-  }
-
-
 
 }
-
-/*
- engage(id:number, subject:string, msgbody:string): Observable<void> {
-    return this.apiservice.post<void>(`${classurl}/${id}/engage`, {subject, msgbody});
-  }
-*/
