cf965bb29834a802d8cc1135c19516db52d6774b
Chialingho <hochialing91@gmail.com>
2019-06-19 09:49:47 +0200

    CLIENT: changing the string to icon for reqInfo

--- CLIENT/src/app/pipe/req-icon.pipe.ts
+++ CLIENT/src/app/pipe/req-icon.pipe.ts
@@ -5,30 +5,25 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ReqIconPipe implements PipeTransform {
 
+  
   transform(value: string[], args:any): string[] {
 //let result: number[];
     let n: string[];
     return n = value.map( (info)=> {
      switch(info) {
-        case 'email': return "far fa-envelope";
-        case 'phone': return "far fa-phone";
-        case 'fname': return "far fa-font";
-        case 'gender': return "far fa-venus-mars";
-        case 'age': return "far fa-envelope";
+        case 'email': return '<i class="fas fa-envelope"></i>';
+        case 'phone': return "fas fa-phone";
+        case 'fname': return "fas fa-font";
+        case 'gender': return "fas fa-venus-mars";
+        case 'age': return "fas fa-envelope";
         
       }
 
     });
-   /* 
-    switch(value) {
-      case ['email']: return 1;
-      case ['phone']: return 2;
-      case ['fname']: return 3;
-      case ['gender']: return 4;
-      case ['age']: return 5;
-     
-    }
-    */
+  
   }
+ 
+
+
 
 }
