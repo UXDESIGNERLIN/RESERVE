7e1f87afa07c0b50dd83bef42caa68edaf0d993d
Chialingho <hochialing91@gmail.com>
2019-06-20 10:14:22 +0200

    CLIENT: reqInfo pipe

--- CLIENT/src/app/pipe/req-icon.pipe.ts
+++ CLIENT/src/app/pipe/req-icon.pipe.ts
@@ -5,22 +5,41 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ReqIconPipe implements PipeTransform {
 
-  
+/*  
   transform(value: string[], args:any): string[] {
 //let result: number[];
     let n: string[];
     return n = value.map( (info)=> {
      switch(info) {
-        case 'email': return '<i class="fas fa-envelope"></i>';
-        case 'phone': return "fas fa-phone";
-        case 'fname': return "fas fa-font";
-        case 'gender': return "fas fa-venus-mars";
-        case 'age': return "fas fa-envelope";
+        case 'email': return '<i class="fas fa-envelope" title="email" ></i>';
+        case 'phone': return '<i class="fas fa-phone" title="phone"></i>';
+        case 'fname': return '<i class="fas fa-font" title="full name"></i>';
+        case 'gender': return '<i class="fas fa-venus-mars" title="gender"></i>';
+        case 'age': return '<i class="fas fa-birthday-cake" title="age"></i>';
         
       }
 
     });
+
+  }
+  */
+  transform(value: string[], args:any): string {
+  //let result: number[];
+    let n: string[];
+    n = value.map( (info)=> {
+      switch(info) {
+        case 'email': return '<i class="fas fa-envelope" title="email"></i>';
+        case 'phone': return '<i class="fas fa-phone" title="phone"></i>';
+        case 'fname': return '<i class="fas fa-font" title="full name"></i>';
+        case 'gender': return '<i class="fas fa-venus-mars" title="gender"></i>';
+        case 'age': return '<i class="fas fa-birthday-cake" title="age"></i>';
+        
+      }
+    })
+
+    return n.join("    ");
   
+
   }
  
 
