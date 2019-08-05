f8b13fce0245cea1f74ef4a580e40b01d6b5a3ca
Ignasi <ignasi@ausva04.com>
2019-08-05 19:09:21 +0200

    CLIENT: Added pending rollcalls notice

--- CLIENT/src/app/pages/main/main.component.ts
+++ CLIENT/src/app/pages/main/main.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { SessionService } from 'src/app/services/session.service';
 import { CompanyService } from 'src/app/services/company.service';
 import { map } from 'rxjs/operators';
+import { TrackingService } from 'src/app/services/tracking.service';
+import { AlertService } from 'src/app/services/alert.service';
 
 @Component({
   templateUrl: 'main.template.html',
@@ -11,12 +13,27 @@ export class MainComponent implements OnInit {
   private username$;
 
   constructor (
-    private companyService: CompanyService
+    private companyService: CompanyService,
+    private trackingService: TrackingService,
+    private alertService: AlertService
   ) {}
   
   ngOnInit(): void {
     this.username$ = this.companyService.getSelf().pipe(
       map((company) => company.name),
     );
+
+    this.trackingService.getPendingRollCall().subscribe((pending: number[]) => {
+      if (pending.length > 0) {
+        this.alertService.confirm(`You've got to roll call`, `You've ${pending.length} schedules pending to be roll called. Do so as promptly as possible in order to gain better system feedback.`, 'Start', 'Later').then((v) => {
+          if (v) {
+            // redirect bla bla bla
+          }
+          else {
+            // esperar d'alguna manera
+          }
+        });
+      }
+    });
   }
 }
--- CLIENT/src/app/services/tracking.service.ts
+++ CLIENT/src/app/services/tracking.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { APIService } from './API.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrackingService {
 
-  constructor() { }
+  constructor (private apiservice: APIService) { }
+
+  getPendingRollCall () {
+    return this.apiservice.get(`rollcall`);
+  }
 }
