4b7225804d157d2e29c6d314ba22104a8e0b30ee
Ignasi <ignasi@ausva04.com>
2019-08-09 12:57:51 +0200

    CLIENT: Requirement icons now displays all icons and highlights the required ones

--- CLIENT/src/app/pipe/req-icon.pipe.ts
+++ CLIENT/src/app/pipe/req-icon.pipe.ts
@@ -5,26 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ReqIconPipe implements PipeTransform {
 
-/*  
-  transform(value: string[], args:any): string[] {
-//let result: number[];
-    let n: string[];
-    return n = value.map( (info)=> {
-     switch(info) {
-        case 'email': return '<i class="fas fa-envelope" title="email" ></i>';
-        case 'phone': return '<i class="fas fa-phone" title="phone"></i>';
-        case 'fname': return '<i class="fas fa-font" title="full name"></i>';
-        case 'gender': return '<i class="fas fa-venus-mars" title="gender"></i>';
-        case 'age': return '<i class="fas fa-birthday-cake" title="age"></i>';
-        
-      }
-
-    });
-
-  }
-  */
+  /*
   transform(value: string[], args:any): string {
-  //let result: number[];
     let n: string[];
     n = value.map( (info)=> {
       switch(info) {
@@ -38,10 +20,21 @@ export class ReqIconPipe implements PipeTransform {
     })
 
     return n.join("    ");
-  
-
   }
- 
+  */
+ transform(value: string[], args:any): string {
+  return [
+    { key: 'email', icon: 'fas fa-envelope', title: 'email' },
+    { key: 'phone', icon: 'fas fa-phone', title: 'phone' },
+    { key: 'fname', icon: 'fas fa-font', title: 'full name' },
+    { key: 'gender', icon: 'fas fa-venus-mars', title: 'gender' },
+    { key: 'age', icon: 'fas fa-birthday-cake', title: 'age' },
+  ]
+  .map((icons) => {
+    return `<i class="${icons.icon} ${value.includes(icons.key) ? 'active-req' : 'inactive-req'}" title="${icons.title}"></i>`
+  })
+  .join('&nbsp;');
+}
 
 
 
